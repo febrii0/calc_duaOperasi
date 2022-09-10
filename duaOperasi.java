c62694288c7a2b81b734534678e04de54d2f4d25
febrii0 <102152647+febrii0@users.noreply.github.com>
2022-09-10 23:52:11 +0700

    Penambahan code calc_duaOperasi
    Menambahkan 3 variabel String yang akan digunakan pada project

--- duaOperasi.java
+++ duaOperasi.java
@@ -17,6 +17,32 @@ import java.util.*;
 public class duaOperasi {
     public static void main(String[] args) {
 
+        String operator;
+        double inputSatu;
+        double inputDua;
 
+        Scanner scanner = new Scanner(System.in);
+        do {
+
+            System.out.println("Masukkan Angka Pertama Kemudian Tekan <Enter>");
+
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Please, enter double");
+                scanner.next();
+
+            }
+            inputSatu = scanner.nextDouble();
+
+            System.out.println("Masukkan Operator (+, -, *, /) lalu Tekan <Enter>");
+
+            operator = scanner.next();
+
+            while (!(operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/"))) {
+                System.out.println("Silahkan Masukkan Operator (+, -, *, /)");
+                operator = scanner.next();
+            }
         }
     }
+}
+
+
