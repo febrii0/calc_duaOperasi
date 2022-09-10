839e2153696df66ac877586bb093b55ad4096062
febrii0 <102152647+febrii0@users.noreply.github.com>
2022-09-10 23:35:45 +0700

    Code program calc_duaOperasi
    File belum lengkap, nanti akan saya lengkapi. Terima kasih.

---
+++ duaOperasi.java
@@ -0,0 +1,22 @@
+package PerhitunganDuaOperasi;
+import java.util.*;
+
+/*
+[1] [2] [3] [4]
+
+[1] Menunjukkan banyaknya operasi yang akan dihitung
+
+[2] Kode operator :
+    1 adalah operasi penjumlahan
+    2 adalah operasi pengurangan
+    3 adalah operasi perkalian
+
+[3] dan [4] Merupakan operan-operan yang akan dioperasikan
+ */
+
+public class duaOperasi {
+    public static void main(String[] args) {
+
+
+        }
+    }
